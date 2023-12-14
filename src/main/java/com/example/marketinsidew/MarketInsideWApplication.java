21acacc6cebf001a254eb5c820b34b417e89a0fe
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-12-14 16:10:58 +0900

    init

---
+++ src/main/java/com/example/marketinsidew/MarketInsideWApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketinsidew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketInsideWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketInsideWApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/marketinsidew/MarketInsideWApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketinsidew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketInsideWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
